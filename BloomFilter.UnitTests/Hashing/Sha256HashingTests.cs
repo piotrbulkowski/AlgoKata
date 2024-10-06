266b91ab85cc2a989e91e803c6d28ae5ba5ce1d7
Piotr Bulkowski <piotrrbulka@gmail.com>
2024-10-06 17:51:09 +0200

    feature: bloomFilter benchmark, fun with micro optimization

---
+++ BloomFilter.UnitTests/Hashing/Sha256HashingTests.cs
@@ -0,0 +1,38 @@
+﻿using BloomFilter.Hashing;
+
+namespace BloomFilter.UnitTests.Hashing;
+
+public class Sha256HashingTests
+{
+    [Fact]
+    public void ComputeHash_ShouldReturnDifferentHash_ForDifferentData()
+    {
+        // Arrange
+        var hashingAlgorithm = new Sha256Hashing();
+        
+        ReadOnlySpan<byte> data1 = [1, 2];
+        ReadOnlySpan<byte> data2 = [3, 4];
+
+        // Act
+        var firstHash = hashingAlgorithm.ComputeHash(data1);
+        var secondHash = hashingAlgorithm.ComputeHash(data2);
+
+        // Assert
+        Assert.NotEqual(firstHash, secondHash);
+    }
+
+    [Fact]
+    public void ComputeHash_ShouldReturnSameHash_ForSameData()
+    {
+        // Arrange
+        var hashingAlgorithm = new Sha256Hashing();
+        ReadOnlySpan<byte> data = [1, 2, 3, 4, 5, 6, 7, 8];
+
+        // Act
+        var firstHash = hashingAlgorithm.ComputeHash(data);
+        var secondHash = hashingAlgorithm.ComputeHash(data);
+
+        // Assert
+        Assert.Equal(firstHash, secondHash);
+    }
+}
--- BloomFilter/Hashing/Sha256Hashing.cs
+++ BloomFilter/Hashing/Sha256Hashing.cs
@@ -4,7 +4,24 @@ namespace BloomFilter.Hashing;
 
 internal sealed class Sha256Hashing : IHashingAlgorithm
 {
+    private static readonly SHA256 _sha256 = SHA256.Create();
     public int ComputeHash(ReadOnlySpan<byte> data)
+    {
+        Span<byte> digest = stackalloc byte[32];
+            
+        _sha256.TryComputeHash(data, digest, out _);
+
+        return BitConverter.ToInt32(digest);
+    }
+    public int ComputeHashFaster(ReadOnlySpan<byte> data)
+    {
+        Span<byte> digest = stackalloc byte[32];
+            
+        _sha256.TryComputeHash(data, digest, out _);
+
+        return (digest[0] << 24) | (digest[1] << 16) | (digest[2] << 8) | digest[3];
+    }
+    public int ComputeHashSlow(ReadOnlySpan<byte> data)
     {
         using var sha256 = SHA256.Create();
         var digest = new byte[sha256.HashSize / 8];
